e30b490c7181c40dbbece831b34bd20be1bccad9
smeyer-ilias <smeyer.ilias@gmx.de>
2017-12-14 13:36:55 +0100

    Increased version

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 // alphanumerical ID of the plugin; never change this
 $id = 'udfcs';
 // code version; must be changed for all code changes
-$version = '5.2.2';
+$version = '5.2.3';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
 $ilias_min_version = "5.2.0";
