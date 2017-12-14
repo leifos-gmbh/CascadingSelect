8ae090cec56a2faa471e5f2c282908c5090a6bc2
smeyer-ilias <smeyer.ilias@gmx.de>
2017-12-14 13:34:34 +0100

    Added db update:

---
+++ sql/dbupdate.php
@@ -0,0 +1,38 @@
+<#1>
+<?php
+
+if(!$ilDB->tableExists('udf_plugin_cselect'))
+{
+	$ilDB->createTable('udf_plugin_cselect',
+		array(	
+			'keyword'       => array(
+				'type'    => 'text',
+				'length'  => 255,
+				'notnull' => false
+				),
+			'keyword_value'        => array(
+				'type'    => 'clob',
+				'notnull' => false
+			),
+		)
+	);
+	$ilDB->addPrimaryKey('udf_plugin_cselect', array('keyword'));
+}
+?>
+
+<#2>
+<?php
+
+$query  = 'select * from settings where module = '. $ilDB->quote('udfd');
+$res = $ilDB->query($query);
+while($row = $res->fetchRow(ilDBConstants::FETCHMODE_OBJECT))
+{
+	$ilDB->insert(
+		'udf_plugin_cselect',
+		array(
+			'keyword' => array('text', $row->keyword),
+			'keyword_value' => array('clob', $row->value)
+		)
+	);
+}
+?>
