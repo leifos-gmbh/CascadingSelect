9d2472da13ba18652e356d8e93858b9c04cb232f
smeyer-ilias <smeyer.ilias@gmx.de>
2018-02-28 16:34:41 +0100

    increased plugin version

--- plugin.php
+++ plugin.php
@@ -2,11 +2,11 @@
 // alphanumerical ID of the plugin; never change this
 $id = 'udfcs';
 // code version; must be changed for all code changes
-$version = '5.2.3';
+$version = '5.3.0';
 // ilias min and max version; must always reflect the versions that should
 // run with the plugin
-$ilias_min_version = "5.2.0";
-$ilias_max_version = "5.2.99";
+$ilias_min_version = "5.3.0";
+$ilias_max_version = "5.3.99";
 
 // optional, but useful: Add one or more responsible persons and a contact email
 $responsible = "Stefan Meyer";
